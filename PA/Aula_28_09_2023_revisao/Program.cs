909ce4f0b0ee4abdb95b000b05a485ef13fb5b52
daniykt <133052952+daniykt@users.noreply.github.com>
2023-09-28 20:46:25 -0300

    Add files via upload

---
+++ PA/Aula_28_09_2023_revisao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula_28_09_2023_revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2, resultado;
+
+            Console.Write("Informe 1° número: ");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe 2° número: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            resultado = n1 + n2;
+            Console.WriteLine($"Resultado da soma: {resultado}");
+
+            resultado = n1 - n2;
+            Console.WriteLine($"Resultado da subtração: {resultado}");
+
+            resultado = n1 * n2;
+            Console.WriteLine($"Resultado da multiplicação: {resultado}");
+
+            resultado = n1 / n2;
+            Console.WriteLine($"Resultado da divisão: {resultado}");
+
+            
+            
+        }
+    }
+}
