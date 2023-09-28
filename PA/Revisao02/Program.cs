eb1fec1f7b1c9088db178b1d26f2477214e6cda1
daniykt <133052952+daniykt@users.noreply.github.com>
2023-09-28 20:47:00 -0300

    Add files via upload

---
+++ PA/Revisao02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Revisao02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.Write("Informe um número: ");
+            valor = int.Parse(Console.ReadLine());
+
+            if (valor == 5)
+                Console.WriteLine("O valor informado é 5");
+
+            else
+                Console.WriteLine("O valor informado é diferente de 5");
+
+           
+            string email;
+
+            Console.Write("Informe o e-mail: ");
+            email = Console.ReadLine();
+
+            if (email == "eduardo@etec.com.br")
+                Console.WriteLine("Acesso liberado");
+
+            else
+                Console.WriteLine("Acesso bloqueado");
+
+
+
+
+         }
+    }
+}
