bdea71bf5fd4407a9fd95ee3aef33fb303ce3576
Aluno <aluno@etec.sp.gov.br>
2023-11-07 22:46:33 -0300

    Aulas do dia 07/11/2023

---
+++ PA/Aula_07_11_2023_Exemplo02For/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula_07_11_2023_Exemplo02For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fator;
+
+            Console.Write("Informe o valor do fator: ");
+            fator = int.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine($"{fator} x {contador}  = {fator * contador}");
+            }
+        }
+    }
+}
---
+++ PA/Aula_07_11_2023_Exemplo03_IF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula_07_11_2023_Exemplo03_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite o Número desejado: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+                Console.WriteLine("O número é par [: ");
+            else
+                Console.WriteLine("O número é impar :)"); 
+
+        }
+    }
+}
