5a5d2e837bd5de44687737855d260e41a486b462
Aluno <aluno@etec.sp.gov.br>
2023-11-14 22:48:48 -0300

    aulas 14/11/2023

---
+++ PA/Aula_14_11_2023_execicios_1_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula_14_11_2023_execicios_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int subtracao1, subtracao2;
+
+
+            for (int contador = 1; contador <= 3; contador++) 
+            {
+                Console.Write("Digite o Primeiro número: ");
+                subtracao1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o Segundo número: ");
+                subtracao2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"O Resultado: {subtracao1 - subtracao2}");
+            }
+        }
+    }
+}
