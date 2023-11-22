db2ed72dc25f5972e9adcfa7195b8fb2e21ac08d
Aluno <aluno@etec.sp.gov.br>
2023-11-21 22:41:48 -0300

    aula do dia 21/11/2023

---
+++ PA/Aula_21_11_2023_Excer1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula_21_11_2023_Excer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura;
+            float maxima = 0, minina = 0;
+
+            for (int contador = 1; contador <= 15; contador++)
+            {
+                Console.Write("Digite uma altura: ");
+                altura = float.Parse(Console.ReadLine());
+
+                if (altura >= maxima)
+                {
+                    maxima = altura;
+
+                }
+                 else if (altura <= minina || minina ==0)
+                {
+
+                    minina = altura;
+                }
+            }
+
+            Console.WriteLine($"Altura maxima: {maxima}");
+            Console.WriteLine($"Altura minina: {minina}");
+        }
+    }
+}
